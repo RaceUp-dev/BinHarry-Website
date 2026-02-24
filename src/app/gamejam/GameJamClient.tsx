6b9effba44776c9d5a92680581025bbd2b76ceb5
WaxyPine <jacqueslucas.m2101@gmail.com>
2026-02-24 22:41:56 +0100

    feat: Ajout de l'html du coeur

--- src/app/gamejam/GameJamClient.tsx
+++ src/app/gamejam/GameJamClient.tsx
@@ -172,12 +172,33 @@ export default function GameJamClient({ editions }: GameJamClientProps) {
               aria-label="Coeur"
               title="Coeur"
             >
-              <svg className="gamejam-reaction-icon" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  d="M12 21s-6.7-4.3-9.2-8.3C.4 9 .9 5.1 4 3.4A5.1 5.1 0 0 1 12 6a5.1 5.1 0 0 1 8-2.6c3.1 1.7 3.6 5.6 1.2 9.3C18.7 16.7 12 21 12 21Z"
-                  fill="currentColor"
+              <div className="heart-container" title="Like">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  id={`heart-${gameId}`}
+                  checked={Boolean(myReaction?.heart)}
+                  readOnly
+                  aria-hidden="true"
+                  tabIndex={-1}
                 />
-              </svg>
+                <div className="svg-container">
+                  <svg viewBox="0 0 24 24" className="svg-outline" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z" />
+                  </svg>
+                  <svg viewBox="0 0 24 24" className="svg-filled" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z" />
+                  </svg>
+                  <svg className="svg-celebrate" width="100" height="100" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <polygon points="10,10 20,20" />
+                    <polygon points="10,50 20,50" />
+                    <polygon points="20,80 30,70" />
+                    <polygon points="90,10 80,20" />
+                    <polygon points="90,50 80,50" />
+                    <polygon points="80,80 70,70" />
+                  </svg>
+                </div>
+              </div>
             </button>
           </div>
         ) : (
